035ce4abf423f2439be1d5c7a7202d12acbff80b
Jorian van Oostenbrugge <jovofdt@gmail.com>
2014-05-14 16:31:52 +0200

    add information as: description, author, creation date to main also add const type qualifier to size and threshold

--- Baseline/Baseline/main.c
+++ Baseline/Baseline/main.c
@@ -1,12 +1,30 @@
-/* Call Library source file */
+/* 
+ * ============================================================================
+ * 
+ *			Filename:	main.c
+ *		 Description:	This program calculates the average (baseline) of an
+ *						array of value's up to the start of a significant
+ *						spike.
+ *
+ *			 Version:	0.1
+ *			 Created:	14-05-2014 16:20:23
+ *
+ *			  Author:	Jorian van Oostenbrugge (jorianvo)
+ *
+ *			   Notes:	Call Library source file. Using labview compatible
+ *						types (int32_t and uint16_t) and headers (extcode.h)
+ *
+ * ============================================================================
+ */
 
 #include "extcode.h"
 
-int32_t determine_baseline(uint16_t adValues[], int32_t size,
-	int32_t threshold, double *baseline);
+// Function declarations
+int32_t determine_baseline(uint16_t adValues[], const int32_t size,
+	const int32_t threshold, double *baseline);
 
-int32_t determine_baseline(uint16_t adValues[], int32_t size,
-	int32_t threshold, double *baseline)
+int32_t determine_baseline(uint16_t adValues[], const int32_t size,
+	const int32_t threshold, double *baseline)
 {
 
 	/* Insert code here */
