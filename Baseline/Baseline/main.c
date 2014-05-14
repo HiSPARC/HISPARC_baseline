8c3b12b8b4820c96be4bd1bbe63c140f783a5bcc
Jorian van Oostenbrugge <jovofdt@gmail.com>
2014-05-14 16:12:33 +0200

    add visual studio project

---
+++ Baseline/Baseline/main.c
@@ -0,0 +1,14 @@
+/* Call Library source file */
+
+#include "extcode.h"
+
+int32_t determine_baseline(uint16_t adValues[], int32_t size,
+	int32_t threshold, double *baseline);
+
+int32_t determine_baseline(uint16_t adValues[], int32_t size,
+	int32_t threshold, double *baseline)
+{
+
+	/* Insert code here */
+
+}
