a9f68118e90f52e5e166722cdcc2c65a74880386
Jorian van Oostenbrugge <jovofdt@gmail.com>
2014-05-15 14:41:12 +0200

    add additional error message (5002) to baseline_c.vi to warn user we cannot accurately determine the baseline because we don't have enough points until the spike. Also add signed int conversion to main.c to avoid errors when calculating the difference and the error message when we don't have sufficient points to calculate the baseline.

--- Baseline/Baseline/main.c
+++ Baseline/Baseline/main.c
@@ -18,15 +18,58 @@
  */
 
 #include "extcode.h"
+#include "stdbool.h"
 
 // Function declarations
 _declspec (dllexport) int32_t determine_baseline(uint16_t adValues[], 
 	const int32_t size, const int32_t threshold, double *baseline);
+bool inRange(const int32_t threshold, int16_t value);
 
 _declspec (dllexport) int32_t determine_baseline(uint16_t adValues[], 
 	const int32_t size, const int32_t threshold, double *baseline)
 {
+	// Declare variables
+	int32_t i;
+	int16_t difference_points;
+
 	// Make sure it is likely we have enough points to calculate the baseline
 	if (size < 50)
 		return (5001);
+	
+	// Go through all elements of the array
+	// Start with second element (1) because we need to compare it to a 
+	// previous element
+	for (i = 1; i < size; i++)
+	{
+		// Determine difference between current and previous point
+		difference_points = (signed int) adValues[i] - adValues[i - 1];
+
+		// Make sure points are within the threshold
+		if (!inRange(threshold, difference_points))
+			break;
+	};
+
+	// We need at least fifty points to accurately determine baseline
+	if (i >= 50)
+	{
+		*baseline = 10.0;
+		return 0;
+	}
+	else
+		return (5002);
+}
+
+/*
+ * Check if a value lies within the absolute value of the supplied threshold
+ * i.e. more than the negative value of the threshold and less than the
+ * positive value of the threshold
+ */
+
+bool
+inRange(const int32_t threshold, int16_t value)
+{
+	if (threshold >= value && value >= (-threshold))
+		return true;
+	else
+		return false;
 }
