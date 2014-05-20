0f1889693abdec841fccc00bf08cee16b5ade5e7
jorianvo <jovofdt@gmail.com>
2014-05-20 15:16:20 +0200

    add clause for all exceptions that could occur like sequence falling out of bounds... Also improved comments

--- running_average.c
+++ running_average.c
@@ -18,7 +18,7 @@
 
 #include <stdio.h>
 
-#define WIDTH 5
+#define WIDTH 8
 
 // Declare functions
 double average(int begin, int end, int array[], int size);
@@ -29,23 +29,17 @@ main (void)
 {
     int array[15] = {550,516,534,531,599,577,543,480,453,419,400,371,356,332,
                        200};
-    int start;
 
     printf("The number is [%i]\n", runningAverage(0, WIDTH, array, 15));
 }
 
 double
-average(int begin, int end, int array[10], int size)
+average(int begin, int end, int array[], int size)
 {
     double sum = 0;
     double average;
     
-//    if (end > (size - 1))
-//    {
-//        begin -= (end - (size - 1));
-//        end = (size - 1);
-//    }
-
+    // Calculate the average
     for (int i = begin; i < end; i++)
     {
         sum += array[i];
@@ -53,35 +47,45 @@ average(int begin, int end, int array[10], int size)
 
     average = sum / WIDTH;
     
-    // DEBUGGING
-    printf("begin = %i\n", begin);
-    printf("end = %i\n", end);
-    printf("average = %f\n", average);
-    printf("____________________________\n");
-    
     return (average);
 }
 
 int
 runningAverage(int start, int end, int array[], int size)
 {
-    if (average(start, end, array, size) < average(start + WIDTH, end + WIDTH, array, size))
-        return (start);
-    else if ((end + WIDTH) == size)
+    int nextStart, nextEnd;
+
+    // Do not exceed array
+    if ((end + WIDTH) > size)
     {
-        // We reached end of array and we know this average is the lowest
-        // so we return the starting point of the final sequence
-        return (start + WIDTH);
+        nextStart = (start + WIDTH) - ((end + WIDTH) - size);
+        nextEnd = size;
+    }
+    else
+    {
+        nextStart = start + WIDTH;
+        nextEnd = end + WIDTH;
+    }
+
+    if (end == size)
+    {
+        // Array exactly size of sequence
+        return start;
+    }
+    else if (average(start, end, array, size) < average(nextStart, nextEnd, array, size))
+    {
+        // The current sequence has a lower average than the next so return
+        return (start);
+    }
+    else if (nextEnd == size)
+    {
+        // A whole number of sequences fits in the array and the last sequence 
+        // is the lowest so we return the starting point of the final sequence
+        return (nextStart);
     }
 
     start += WIDTH;
     end += WIDTH;
 
-    // DEBUGGING
-    printf("=======================\n");
-    printf("start = %i\n", start);
-    printf("end = %i\n", end);
-    printf("=======================\n");
-        
     return (runningAverage(start, end, array, size));
 }
