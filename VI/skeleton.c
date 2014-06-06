e5fb7bbb7f961668fcbd1d140650aaea55b477e4
Jorian van Oostenbrugge <jovofdt@gmail.com>
2014-06-06 16:38:47 +0200

    update C skeleton in baseline_vi so also created skeleton

---
+++ VI/skeleton.c
@@ -0,0 +1,15 @@
+/* Call Library source file */
+
+#include "extcode.h"
+
+int32_t findBaseline(uint32_t start, uint32_t end, uint16_t array[], 
+	uint32_t size, int32_t threshold, uint32_t width, double *baseline);
+
+int32_t findBaseline(uint32_t start, uint32_t end, uint16_t array[], 
+	uint32_t size, int32_t threshold, uint32_t width, double *baseline)
+{
+
+	/* Insert code here */
+
+}
+
