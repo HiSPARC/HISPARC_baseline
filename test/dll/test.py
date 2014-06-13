474038efab9c282a17d783145fc4b78e76b3c269
Jorian van Oostenbrugge <jovofdt@gmail.com>
2014-06-13 16:19:43 +0200

    update test code to not only print errors but also count them

--- test/dll/test.py
+++ test/dll/test.py
@@ -1,8 +1,16 @@
 from ctypes import *
 from sys import exit
+import time
+import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 from get_trace_and_baseline import get_traces_baseline
 
+def save_txt(timestr, trace):
+	FILE = "trace_" + timestr + ".csv"
+	np.savetxt(FILE, trace, delimiter=",")
+	
 # Set types of the pointers to baseline and stdev
 pBaseline = c_int16()
 pStdev = c_int16()
@@ -32,20 +40,33 @@ for x, (t, b, s) in enumerate(get_traces_baseline()):
 		dll_return = findBaseline(0, len(trace), trace_array, len(trace), 15, 100, pBaseline, pStdev)
 		
 		if pBaseline.value != bsl or  pStdev.value != stdev:
-			print("--------------------Next-----------------")
+			print("-------------------Next-----------------")
 			
 		if pBaseline.value != bsl:
+			timestr = str(time.time())
+			save_txt(timestr, trace)
+			plt.figure()
+			plt.plot(trace)
+			plt.ylim([100,900])
+			plt.title(timestr)
 			print bsl
 			print pBaseline.value
 			count_bsl_err += 1
 		
 		if pStdev.value != stdev:
+			timestr = str(time.time())
+			save_txt(timestr, trace)
+			plt.figure()
+			plt.plot(trace)
+			plt.ylim([100,900])
+			plt.title(timestr)
 			print stdev
 			print pStdev.value
 			count_stdev_err += 1
 		
 		counter += 1
 		
+plt.show()	
 print ("Total traces = ", counter)
 print ("Total mismatches bsl = ", count_bsl_err)
 print ("Total mismatches stdev = ", count_stdev_err)
