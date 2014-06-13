898133e020600d8653d08aab8d903896b5742760
Jorian van Oostenbrugge <jovofdt@gmail.com>
2014-06-13 14:27:56 +0200

    add python testing code to be able to call dll directly from python

---
+++ test/dll/get_trace_and_baseline.py
@@ -0,0 +1,28 @@
+import zlib
+import tables
+import numpy as np
+
+DATAPATH = '2014_4_5.h5'
+
+def get_traces_baseline(station_group='/hisparc/cluster_amsterdam/station_501'):
+    with tables.open_file(DATAPATH, 'r') as data:
+        station = data.get_node(station_group)
+        events = station.events
+        blobs = station.blobs
+        for event in events:
+			traces = get_traces_for_event(blobs, event)
+			baselines = [b for b in event['baseline'] if not b == -1]
+			std_devs = [s for s in event['std_dev'] if not s == -1]
+			yield traces, baselines, std_devs
+
+def get_traces_for_event(blobs, event):
+	trace_ids = [trace_id for trace_id in event['traces'] if not trace_id == -1]
+	traces = [get_trace(blobs, id) for id in trace_ids]
+	return traces
+
+def get_trace(blobs, id):
+	return [int(x) for x in zlib.decompress(blobs[id]).split(',') if not x == '']
+
+	
+if __name__ == '__main__':
+    get_trace_baseline(1, 1)
---
+++ test/dll/test.py
@@ -0,0 +1,53 @@
+from ctypes import *
+from sys import exit
+
+from get_trace_and_baseline import get_traces_baseline
+
+# Set types of the pointers to baseline and stdev
+pBaseline = c_int16()
+pStdev = c_int16()
+
+# Make sure we can use the function from baseline.dll
+baseline_dll = CDLL("../../project/Baseline/Debug/Baseline.dll")
+findBaseline = baseline_dll.findBaseline
+
+count_bsl_err = 0
+count_stdev_err = 0
+counter = 0
+
+# Iterate over all events. Default is station 501 at Science Park
+for x, (t, b, s) in enumerate(get_traces_baseline()):
+
+	if x == 10:
+		break
+		
+	for trace, bsl, stdev in zip(t, b, s):
+		# Define array
+		trace_array = (c_uint16 * len(trace))(*trace)
+		
+		# Define function prototype as used in dll
+		findBaseline.argtypes = [c_int32, c_int32, c_uint16 * len(trace_array), c_int32, c_uint16, c_int32, POINTER(c_int16), POINTER(c_int16)]
+		findBaseline.restype = c_int32
+		
+		dll_return = findBaseline(0, len(trace), trace_array, len(trace), 15, 100, pBaseline, pStdev)
+		
+		if pBaseline.value != bsl or  pStdev.value != stdev:
+			print("--------------------Next-----------------")
+			
+		if pBaseline.value != bsl:
+			print bsl
+			print pBaseline.value
+			count_bsl_err += 1
+		
+		if pStdev.value != stdev:
+			print stdev
+			print pStdev.value
+			count_stdev_err += 1
+		
+		counter += 1
+		
+print ("Total traces = ", counter)
+print ("Total mismatches bsl = ", count_bsl_err)
+print ("Total mismatches stdev = ", count_stdev_err)
+
+exit()
